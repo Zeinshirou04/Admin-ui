5f71ba0b798e2d7977af887b7f9f258884365479
Zayn <Farras.FF6@gmail.com>
2024-12-02 17:40:59 +0700

    Commit Deletion
    - Deleted Layout Folder

--- src/layout/GuestLayout.jsx
+++
@@ -1,9 +0,0 @@
-function GuestLayout({ children }) {
-    return (
-        <main className="bg-main w-svw h-svh tracking-wide font-inter">
-            {children}
-        </main>
-    );
-}
-
-export default GuestLayout;
